2fbc3af7131c47f68842ef61d630f520716cd5e4
Joanne Bogart <jrb@slac.stanford.edu>
2025-10-03 11:09:07 -0700

    Bug fix. Top file was not getting rewritten when it should have been

--- skycatalogs_creator/utils/config_creator_utils.py
+++ skycatalogs_creator/utils/config_creator_utils.py
@@ -287,17 +287,17 @@ class ConfigWriter:
                 # No change necessary
                 return
 
-            # Otherwise need to add or modify value for our object type
-            # First have to fix values for any other object types already
-            # mentions.  Value read in looks like "!include an_obj_type.yaml"
-            for k, v in top['object_types'].items():
-                cmps = v.split(' ')
-                new_value = IncludeValue(cmps[1])
-                top['object_types'][k] = new_value
+        # Otherwise need to add or modify value for our object type
+        # First have to fix values for any other object types already
+        # mentions.  Value read in looks like "!include an_obj_type.yaml"
+        for k, v in top['object_types'].items():
+            cmps = v.split(' ')
+            new_value = IncludeValue(cmps[1])
+            top['object_types'][k] = new_value
 
-            top['object_types'][object_type] = value
-            self.update_yaml(top, top_path)
-            return
+        top['object_types'][object_type] = value
+        self.update_yaml(top, top_path)
+        return
 
         # Write out top file from scratch, ignoring other object types
         # which may have been referenced in older version
