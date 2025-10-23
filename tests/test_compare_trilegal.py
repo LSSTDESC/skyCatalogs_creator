ae085d170840b756b2ced9fc3b7d456ca63c17f0
Joanne Bogart <jrb@slac.stanford.edu>
2025-10-23 11:09:15 -0700

    add test for comparing trilegal catalogs to standard

---
+++ tests/test_compare_trilegal.py
@@ -0,0 +1,75 @@
+"""
+Unit tests for skyCatalogs_creator API comparing created files to
+standard.
+"""
+
+import unittest
+import os
+# from pathlib import Path
+import numpy as np
+import pyarrow.parquet as pq
+from .utilities import compare, write_selected
+
+
+class TrilegalCompare(unittest.TestCase):
+
+    def setUp(self):
+        '''
+        Nothing to do
+        '''
+        pass
+
+    def tearDown(self):
+        pass                  # nothing to do
+
+    def extracted_indices(self, file):
+        '''
+        Parameters
+        ----------
+        file    (string)  path to a parquet file
+
+        Returns
+        -------
+        List of int.   Indices of rows in file w.r.t. original catalog
+                       file was extracted from
+        '''
+        pq_file = pq.ParquetFile(file)
+        ids = np.array(pq_file.read_row_group(0, columns=['id'])['id'])
+        ixes = [int(id.split('_')[-1:][0]) for id in ids]
+
+        return ixes
+
+    def testcompare_trilegal(self):
+        '''
+        Compare newly generated main and flux files to the standard ones
+        Stored under data/ci_sample.   But first have to sparsify new
+        ones so rows match those in the standard ones.
+        '''
+
+        # Find the newly-generated files
+        new_dir = os.getenv('CI_DATA')
+        pixel = os.getenv('TRILEGAL_PIXEL')
+
+        # Find dir for standard files
+        standard_dir = os.path.join(new_dir, '..', 'ci_sample')
+
+        main_name = f'trilegal_{pixel}.parquet'
+        flux_name = f'trilegal_flux_{pixel}.parquet'
+
+        standard_main = os.path.join(standard_dir, main_name)
+        standard_flux = os.path.join(standard_dir, flux_name)
+
+        # sparsify
+        ixes = self.extracted_indices(standard_main)
+        sparse_main = os.path.join(new_dir, 'sparse_' + main_name)
+        write_selected(standard_main, sparse_main, ixes)
+        sparse_flux = os.path.join(new_dir, 'sparse_' + flux_name)
+        write_selected(standard_flux, sparse_flux, ixes)
+
+        # compare
+        compare(standard_main, sparse_main)
+        compare(standard_flux, sparse_flux, cat_type='flux')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/utilities.py
@@ -0,0 +1,101 @@
+from collections import OrderedDict
+import pyarrow.parquet as pq
+import pyarrow as pa
+import pandas as pd
+import numpy as np
+import numpy.ma as ma
+
+
+def compare(file1, file2, object_type='trilegal', cat_type='main',
+            exact=True):
+    if object_type == 'pointsource':
+        obj = 'star'
+    else:
+        obj = object_type
+
+    cols = []
+    if cat_type == 'flux':
+        cols.append('lsst_flux_r')
+        if obj.endswith('galaxy'):
+            cols.append('galaxy_id')
+        else:
+            cols.append('id')
+    else:      # main
+        cols.extend(['ra', 'dec'])
+        if obj.endswith('galaxy'):
+            cols.extend(['galaxy_id', 'MW_av', 'redshift'])
+        elif obj == 'star':
+            cols.extend(['id', 'MW_av', 'magnorm'])
+        elif obj == 'sso':
+            cols.extend(['id', 'mjd', 'trailed_source_mag'])
+        elif obj == 'trilegal':
+            cols.extend(['id', 'logT', 'imag'])
+
+    # Keep track of which columns are floating point.  Exact match might
+    # not be a reasoable condition
+    floats = ['ra', 'dec', 'MW_av', 'redshift', 'magnorm',
+              'trailed_source_mag', 'logT', 'imag', 'lsst_flux_r']
+    pq_file1 = pq.ParquetFile(file1)
+    pq_file2 = pq.ParquetFile(file2)
+
+    assert pq_file1.metadata.num_rows == pq_file2.metadata.num_rows, 'Same number of rows'
+
+    # if args.debug:
+    #     print("Number of rows is identical")
+
+    # Number of rows should be small so it's safe to assume a single row group.
+    tbl1 = pq_file1.read_row_group(0, columns=cols)
+    tbl2 = pq_file2.read_row_group(0, columns=cols)
+
+    for c in cols:
+        assert tbl1[c] == tbl2[c], f'Column {c} is identical'
+        # For floats should optionally check instead if they're close
+
+        # if args.debug:
+        #    print(f"Column {c} is identical")
+
+
+def write_selected(input_path, output_path, selected):
+    '''
+    Parameters
+    ----------
+    input_path    string     Input parquet file to be sparsified
+    output_path   string
+    selected      list(int)   Which rows to preserve in output
+    '''
+
+    in_file = pq.ParquetFile(input_path)
+    n_gp = in_file.metadata.num_row_groups
+    schema = in_file.schema
+    rgp = in_file.read_row_group(0)
+
+    # file schema consists of column schemas. If the column has structure
+    # the name we want is at the start of column schema 'path' attribute
+    out_dict = OrderedDict({col.path.split('.')[0]: [] for col in schema})
+    offset = 0
+    i_gp = 0
+
+    while i_gp < n_gp:
+        rgp = in_file.read_row_group(i_gp)
+        rg_len = len(rgp)
+        rgp_selected = [(i - offset) for i in selected if i >= offset and i < offset + rg_len]
+        if len(rgp_selected) > 0:
+            # mask off everything except random selection
+            m_l = [i not in rgp_selected for i in range(rg_len)]
+            m = ma.make_mask(m_l)
+
+            for k in out_dict.keys():
+                more = ma.MaskedArray(np.array(rgp[k]), m).compressed()
+                out_dict[k] += list(more)
+
+        i_gp += 1
+        offset += rg_len
+
+    arrow_schema = schema.to_arrow_schema()
+    writer = pq.ParquetWriter(output_path, arrow_schema)
+
+    out_df = pd.DataFrame.from_dict(out_dict)
+    out_table = pa.Table.from_pandas(out_df, schema=arrow_schema)
+
+    writer.write_table(out_table)
+    writer.close()
